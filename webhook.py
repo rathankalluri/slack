da8fd6fc4e9eace720c366e691d17818c443632f
rathankalluri <ratan.kalluri@gmail.com>
2018-03-16 18:24:55 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,5 +1,11 @@
 import requests
 import json
 
-data = request.POST
-print data["challenge"]
+def responder(request.POST):
+  data = request.POST
+  if data["challenge"]:
+    return data["challenge"]
+  else:
+    retrun "Error!!"
+
+responder(request.POST)
